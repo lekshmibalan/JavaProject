1b49c752171bb17db8e7d54a66fecb9f29d11231
lekshmi <lekshmi@kranadmin>
2025-06-27 17:27:12 +0530

    Datatypes

---
+++ JavaWorkspace/src/day2/DataTypesDemo.java
@@ -0,0 +1,29 @@
+package day2;
+
+public class DataTypesDemo {
+	public static void main(String[] args) {
+		// Numeric data types
+		int a=100, b= 200;
+		System.out.println("The value of a is:"+a);
+		System.out.println("The value of a is:"+b);
+		System.out.println("The sum of a and b is:" +(a+b));
+		
+		char grad='A';
+		System.out.println(grad);
+		
+		double dbl=1234.4343412;
+		System.out.println(dbl);
+		
+		
+		String name="Lekshmi";
+		System.out.println(name); 
+		
+		long l=123456789L;
+		System.out.println(l);
+	
+		boolean bl=true;
+		System.out.println(bl);
+	
+	}
+	
+	}
