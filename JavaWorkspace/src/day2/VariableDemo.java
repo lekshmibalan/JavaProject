69849b28e4c6257a72176989d51e434c4be7ffb6
lekshmi <lekshmi@kranadmin>
2025-06-27 14:35:21 +0530

    First Project

---
+++ JavaWorkspace/src/day2/VariableDemo.java
@@ -0,0 +1,33 @@
+package day2;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		//int a;     //declaration
+		//a=100;     //assignment
+		
+		/*
+		 int a=100; //declaration+ assignment
+		 System.out.println(a);
+		a=300;
+		System.out.println(a); */
+		
+		//Approach 1
+		 int a=100;
+		 int b=200;
+		 int c=300;
+		 /*System.out.println(a);
+		 System.out.println(b);
+		 System.out.println(c); 
+		 
+		 System.out.println("The value of a is:" +a);
+		 System.out.println("The value of a is:" +b);
+		 System.out.println("The value of a is:" +c); */
+		 
+		 System.out.println(a+" "+b+" "+c); 
+		 
+		
+
+	}
+
+}
