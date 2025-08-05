3cc8c3f21528561865f85a3169e1c192359ca753
lekshmi <lekshmi@kranadmin>
2025-08-05 11:03:33 +0530

    overloadingVsoverriding

---
+++ JavaWorkspace/src/inheritance/OverloadingVsOverriding.java
@@ -0,0 +1,42 @@
+package inheritance;
+
+class ABC
+{
+	void m1(int a)
+	{
+		System.out.println(a);
+	}
+
+void m2(int b)
+{
+	System.out.println(b);
+}
+}
+
+class XYZ extends ABC
+{
+	void m1(int a) //Overriding
+	{
+		System.out.println(a*a);
+	}
+	void m2(int b) //overrided
+	{
+		System.out.println(b*b);
+	}
+	void m2(int a,int b) // Overloading
+	{
+		System.out.println(a+b);
+	}
+}
+
+public class OverloadingVsOverriding {
+
+	public static void main(String[] args) {
+		XYZ xyzobj=new XYZ();
+		xyzobj.m1(10);
+		xyzobj.m2(20);
+		xyzobj.m2(10, 20);
+	
+	}
+
+}
