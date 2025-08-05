b1fa2882d25e2a2dba6476a3dfe11fe5f0914eee
lekshmi <lekshmi@kranadmin>
2025-08-05 12:32:55 +0530

    superkeyword

---
+++ JavaWorkspace/src/inheritance/SuperKeyword.java
@@ -0,0 +1,26 @@
+package inheritance;
+
+class Animal
+{
+	String color="white";
+}
+
+class Dog extends Animal
+{
+	String color="black";
+	void displayColor()
+	{
+		System.out.println(super.color);
+	}
+}
+
+
+public class SuperKeyword {
+
+	public static void main(String[] args) {
+	Dog d=new Dog();
+	d.displayColor();
+
+	}
+
+}
