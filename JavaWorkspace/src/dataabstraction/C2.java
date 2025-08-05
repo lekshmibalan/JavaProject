af53961f28cf02554afc2c78f44d972bf9c924c4
lekshmi <lekshmi@kranadmin>
2025-08-05 17:28:43 +0530

    multiple inheritance - Interface

---
+++ JavaWorkspace/src/dataabstraction/C2.java
@@ -0,0 +1,10 @@
+package dataabstraction;
+
+public class C2 {
+ int z=300;
+ void m3()
+ {
+	 System.out.println(z);
+ }
+
+}
---
+++ JavaWorkspace/src/dataabstraction/I2.java
@@ -0,0 +1,6 @@
+package dataabstraction;
+
+public interface I2 {
+	int y=200;
+	void m2(); // abstract method
+}
