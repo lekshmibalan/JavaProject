27336057f4b15813e089b6b2442158cc177abb41
lekshmi <lekshmi@kranadmin>
2025-08-07 13:45:55 +0530

    typecasting

---
+++ JavaWorkspace/src/typecasting/TypeCastingConcepts.java
@@ -0,0 +1,28 @@
+package typecasting;
+//Upcasting - converting value from smaller to larger
+//int ---> long
+//float --->double
+
+//Downcasting - converting value from larger to smaller
+//long --->int
+//double --->float
+public class TypeCastingConcepts {
+
+	public static void main(String[] args) {
+		// Upcasting -Automatic --Smaller to Larger
+		/* int intvalue=100;
+		long longvalue=intvalue;
+		System.out.println(longvalue);
+		*/
+		//float floatvalue=10.5F;
+		//double doublevalue=floatvalue;
+		//Downcasting -Manually -- larger to smaller
+	//long longvalue=10000;
+	//int intvalue=(int)longvalue;
+	
+		double doublevalue=125.55;
+		float floatvalue=(float)doublevalue;
+
+	}
+
+}
